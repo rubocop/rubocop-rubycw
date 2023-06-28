4031ef34113a60d60db8382e814a7cf491fa353f
Koichi ITO <koic.ito@gmail.com>
2023-06-28 12:21:12 +0900

    Fix build errors
    This commit fixes the following build erors:
    
    ```console
    $ bundle exec rake
    (snip)
    
    Failures:
    
      1) RuboCop::Cop::Rubycw::Rubycw registers an offense with ruby -cw
         Failure/Error:
               expect_offense(<<~RUBY)
                 1
                 ^ unused literal ignored
                 p 1
    
                 p if /re/
                 ^ regex literal in condition
               RUBY
    
           Diff:
    
           @@ -1,7 +1,7 @@
            1
           -^ unused literal ignored
           +^ Rubycw/Rubycw: unused literal ignored
            p 1
    
            p if /re/
           -^ regex literal in condition
           +^ Rubycw/Rubycw: regex literal in condition
    
         # ./spec/rubocop/cop/rubycw/rubycw_spec.rb:9:in `block (2 levels) in <top (required)>'
    
      2) RuboCop::Cop::Rubycw::Rubycw registers an offense for duplicated range of regexp
         Failure/Error:
           expect_offense(<<~RUBY)
             /[aaa]/
             ^ character class has duplicated range: /[aaa]/
           RUBY
    
           Diff:
           @@ -1,3 +1,3 @@
            /[aaa]/
           -^ character class has duplicated range: /[aaa]/
           +^ Rubycw/Rubycw: character class has duplicated range: /[aaa]/
    
         # ./spec/rubocop/cop/rubycw/rubycw_spec.rb:20:in `block (2 levels) in <top (required)>'
    
    Finished in 0.04322 seconds (files took 1.11 seconds to load)
    4 examples, 2 failures
    
    Failed examples:
    
    rspec ./spec/rubocop/cop/rubycw/rubycw_spec.rb:8
    # RuboCop::Cop::Rubycw::Rubycw registers an offense with ruby -cw
    rspec ./spec/rubocop/cop/rubycw/rubycw_spec.rb:19
    # RuboCop::Cop::Rubycw::Rubycw registers an offense for duplicated range of regexp
    ```

--- spec/rubocop/cop/rubycw/rubycw_spec.rb
+++ spec/rubocop/cop/rubycw/rubycw_spec.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe RuboCop::Cop::Rubycw::Rubycw do
-  subject(:cop) { described_class.new(config) }
-
-  let(:config) { RuboCop::Config.new }
-
+RSpec.describe RuboCop::Cop::Rubycw::Rubycw, :config do
   it 'registers an offense with ruby -cw' do
     expect_offense(<<~RUBY)
       1
